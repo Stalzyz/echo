d11978d587330bef8cf7717fd399bb70c3988158
Stalzyz <mstalinkumar05@gmail.com>
2026-09-25 11:32:10 +0530

    fix: resolve prisma/bcrypt in sync-demo

--- scripts/sync-demo.js
+++ scripts/sync-demo.js
@@ -1,5 +1,5 @@
-const { PrismaClient } = require("./packages/db/node_modules/@prisma/client");
-const bcrypt = require("./apps/web/node_modules/bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
 const prisma = new PrismaClient();
 
 async function main() {
